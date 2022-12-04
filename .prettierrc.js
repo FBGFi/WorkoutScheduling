cb27f76fb4dbeb7e87c3ed61eeb9f3b9dddbd400
Aleksi Alhola <aleksi.alhola@gmail.com>
2022-12-04 13:10:29 +0200

    initialize project

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: false,
+  trailingComma: 'all',
+  printWidth: 80,
+  indent: 2,
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactNative from "react-native";
+
+const App = () => {
+  return (
+    <ReactNative.View>
+      <ReactNative.Text>Hi</ReactNative.Text>
+    </ReactNative.View>
+  );
+};
+
+export default App;
