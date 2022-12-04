7bd69712542513f7d92924a392f583c20c833dae
Aleksi Alhola <aleksi.alhola@gmail.com>
2022-12-04 15:30:04 +0200

    basic styling

---
+++ src/styles/colors.ts
@@ -0,0 +1,21 @@
+export const Colors = {
+  /* Shades of green */
+  primary: "rgb(95,240,74)",
+  primary_light: "rgb(101,255,79)",
+  primary_dark: "rgb(68,171,53)",
+  primary_font: "rgb(23,59,18)",
+
+  /* Shades of red */
+  secondary: "rgb(229,72,72)",
+  secondary_light: "rgb(255,80,80)",
+  secondary_dark: "rgb(190,60,60)",
+  secondary_font: "rgb(64,20,20)",
+
+  /* Background colors */
+  primary_background: "rgb(240,250,230)",
+
+  /* Unused but cool colors */
+  unused: "rgb(229,171,53)",
+};
+
+export default Colors;
---
+++ src/styles/dimensions.ts
@@ -0,0 +1,11 @@
+export const fontSizes = {
+  font_small: 20,
+  font_regular: 26,
+  font_large: 32,
+};
+
+export const Dimensions = {
+  ...fontSizes,
+};
+
+export default Dimensions;
