7f54ab3a2fac245bf0ba044546e2ad755809321d
Aleksi Alhola <aleksi.alhola@gmail.com>
2022-12-04 16:06:10 +0200

    font configuration

---
+++ src/styles/fonts.ts
@@ -0,0 +1,9 @@
+export const Fonts = {
+  Staatliches: "Staatliches-Regular",
+  Titillium: "TitilliumWeb-Regular",
+  Titillium_cursive: "TitilliumWeb-Italic",
+  Titillium_semibold: "TitilliumWeb-SemiBold",
+  Titillium_bold: "TitilliumWeb-Bold",
+};
+
+export default Fonts;
